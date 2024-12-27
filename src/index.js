512c4ffc26c4f2241d9a41cffa17ae6eed5ef973
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-27 11:12:26 +0100

    add index.js to set up ReactDOM and routing with Bootstrap styles

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import './scss/style.scss';
+
+/** ================ TITLE ====================== */
+import HomePagdData from './data/HomePagdData.json';
+const { hero } = HomePagdData;
+document.title = hero ? hero.name : "PortFolio";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
